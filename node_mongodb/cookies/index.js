3f61ad96fe528731180ddbeee37b8d358165527f
Thiha <yairthiha63@gmail.com>
2022-07-27 21:52:58 +0630

    update

--- node_mongodb/cookies/index.js
+++ node_mongodb/cookies/index.js
@@ -1,19 +1,22 @@
 var cookieParser = require('cookie-parser');
-app.use(cookieParser());
+
 
 var express = require('express');
 var app = express();
-app.use(express.json())
-
+//app.use(express.json());
+app.use(cookieParser());
 app.get('/', function (req, res) {
-  res.cookie('name', 'express').send('cookie set'); //sets name = express
+  res.cookie('name', 'express').send('cookie set');
+  //res.cookie('name', 'value', { expire: 360000 + Date.now() });
+   //sets name = express
+   console.log('Cookies: ', req.cookies);
 }); 
 
 //Expires after 360000 ms from the time it is set.
-res.cookie(name, 'value', { expire: 360000 + Date.now() });
+
 
 //This cookie also expires after 360000 ms from the time it is set.
-res.cookie(name, 'value', { maxAge: 360000 });
+//res.cookie(name, 'value', { maxAge: 360000 });
 
 app.get('/clear_cookie_foo', function(req, res){
   res.clearCookie('foo');
--- node_mongodb/person.js
+++ node_mongodb/person.js
@@ -1,11 +1,34 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyparser = require('body-parser');
+const pug = require('pug');
 var app = express();
+var urlencodedParser = bodyparser.urlencoded({ extended: true});
 
-app.use(bodyparser.urlencoded({ extended: false }));
+//app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+app.set('view engine', 'pug');
 
+//mongoose.connect('mongodb+srv://Thiha:693675232@cluster0.sg54i9d.mongodb.net/my_db', {
+//   useNewUrlParser: true
+//},
+//   err => {
+//       if (!err) {
+//           console.log('Connection succeeded');
+//       } else {
+//           console.log('Error in connection' + err);
+//       }
+//   });
+mongoose.connect('mongodb://0.0.0.0:27017/my_db', {
+   useNewUrlParser: true
+},
+   err => {
+       if (!err) {
+           console.log('Connection succeeded');
+       } else {
+           console.log('Error in connection' + err);
+       }
+   });
 var personSchema = mongoose.Schema({
   name: String,
   age: Number,
@@ -13,26 +36,41 @@ var personSchema = mongoose.Schema({
 });
 
 var Person = mongoose.model("Person", personSchema);
+app.get('/', function (req, res) {
+   res.sendFile(__dirname+"/person.html");
+});
 
-app.post('/person', function(req, res){
-   var personInfo = req.body; //Get the parsed information
-   console.log('personInfo', personInfo);  
-  if(!personInfo.name || !personInfo.age || !personInfo.nationality){
-     res.json({message: "Sorry, you provided worng info", type: "error"});
-  } else {
-     var newPerson = new Person({
-        name: personInfo.name,
-        age: personInfo.age,
-        nationality: personInfo.nationality
-     });
+app.post('/person', urlencodedParser, function (req, res) {
+   console.log("some text");
+   var personInfo =
+    {
+       name:req.body.name,
+       age: req.body.age,
+       nationality:req.body.nationality
+   };
    
-     newPerson.save(function(err, Person){
-        if(err)
-           res.json({message: "Database error", type: "error"});
-        else
-           res.json({message: "New person added", type: "success", person: personInfo});
-     });
-  }
+   if (!personInfo ){
+       res.render('show_message', {
+           message: "Sorry, you provided worng info", type: "error"
+       });
+   } else {
+       var newPerson = new Person({
+           name: personInfo.name,
+           age: personInfo.age,
+           nationality: personInfo.nationality
+       });
+      newPerson.save(function (err, Person) {
+           if (err)
+               res.render('show_message', { message: "Database error", type: "error" });
+           else
+               res.render('show_message', {
+                   message: "New person added", type: "success", person: personInfo
+
+               });
+       });
+   }
+   var jsonString=JSON.parse(JSON.stringify(personInfo));
+   console.log(jsonString);
 });
 
 app.listen(3000, () => {
