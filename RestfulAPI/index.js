b918af3bfcbf05eb3e98df7f58dd88c7b8044f54
Thiha <yairthiha63@gmail.com>
2022-08-01 23:02:08 +0630

    2022/08/01 update

--- RestfulAPI/index.js
+++ RestfulAPI/index.js
@@ -4,14 +4,22 @@ var multer = require('multer');
 var upload = multer();
 var cookieParser = require('cookie-parser');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://0.0.0.0:27017/movie_db');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
+app.get('/', (req, res) => {
+  res.send('Hello Kitty');
+});
+
 
 //Require the Router we defined in movies.js
 var movies = require('./movies.js');
+const { connect } = require('./movies.js');
+//const { default: mongoose } = require('mongoose');
 
 //Use the Router on the sub route /movies
 app.use('/movies', movies);
